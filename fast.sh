e6e9bcedd4241b5e6cc0ac86598ef59c630b0abb
Devender <49671176+raoshaab@users.noreply.github.com>
2022-04-05 11:29:58 +0530

    Update fast.sh

--- fast.sh
+++ fast.sh
@@ -1,6 +1,19 @@
 #! /usr/bin/env bash
 
 
+#banner
+
+echo -e "\e[35m "   
+printf "   __   _  \n"            _                                            
+printf "  / _| | |               | |                                           \n"
+printf " | |_  | |   __ _   ___  | |__    ______   _ __    ___   _ __     ___  \n"
+printf " |  _| | |  / _  | / __| | '_ \  |______| | '__|  / _ \ | '_ \   / _ \ \n"
+printf " | |   | | | (_| | \__ \ | | | |          | |    |  __/ | |_) | | (_) |\n"
+printf " |_|   |_|  \__'_| |___/ |_| |_|          |_|     \___| | .__/   \___/ \n"
+printf "                                                        | |            \n"
+printf "                                                        |_|            \n"
+echo -e "\e[0m"
+
 #checking  If OS is Kali-Linux or other
 
 o=$(grep "Kali GNU/Linux" /etc/os-release -o|wc -m)
@@ -12,25 +25,23 @@ car='/tmp/fast-repo-dev'
 mkdir $car 2>/dev/null && cd $car 2>/dev/null
 curl -s http://http.kali.org/README.mirrorlist -k |grep "this country" -A 20|grep http|cut -d '/' -f3 > mirror
 a=$(cat mirror)
-echo ' Checking the Best Mirror ;)  Hold on '
+echo -e  '\e[32m Checking the Best Mirror ;)  Hold on ヽ(´▽`)/  \e[0m'
 echo -e '\n\n\n' 
 #checking the mirror latency 
 k=$(echo $a|wc -l)
+
 for i in  $(cat mirror)
 do
 ping -c4 $i|grep rtt |cut -d '/' -f5 >>new 
-
-  for((j=1; j<=k; j++))
-   do
-    echo -n "......."
-   done
+echo -n "......."
 done
 
 #combine result 
+
 paste mirror new > final
 repo=$(cat final|awk '{print NF, $0}' | sort -k1,1rn -k3 -n | cut -d' ' -f2-|head -n1|awk '{print $1}')
 
-echo -e "\n\n\nNow we have Set best Mirror Repo to you Kali-Linux\n\n Try Now  sudo apt update    \n Happy Hacking :) @" 
+echo -e "\n\n\n \e[96Now we have Set best Mirror Repo to you Kali-Linux\n\n Try Now  sudo apt update    \n Happy Hacking :) @raoshaab" 
  
 sudo sed -i 's/^/#/g' "/etc/apt/sources.list"
 echo "deb http://$repo/kali kali-rolling main contrib non-free" |sudo tee -a /etc/apt/sources.list >/dev/null 
