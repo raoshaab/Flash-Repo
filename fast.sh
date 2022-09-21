a475ac4e48aab5daa3fe9da77c9232eceb8f4946
Devender <49671176+raoshaab@users.noreply.github.com>
2022-09-21 11:21:04 +0530

    Ubuntu support added +

--- fast.sh
+++ fast.sh
@@ -1,48 +1,103 @@
 #! /usr/bin/env bash
-
-
 #banner
 
-echo -e "\e[92m "   
-printf "   __   _  \n"            _                                            
-printf "  / _| | |               | |                                           \n"
-printf " | |_  | |   __ _   ___  | |__    ______   _ __    ___   _ __     ___  \n"
-printf " |  _| | |  / _  | / __| | '_ \  |______| | '__|  / _ \ | '_ \   / _ \ \n"
-printf " | |   | | | (_| | \__ \ | | | |          | |    |  __/ | |_) | | (_) |\n"
-printf " |_|   |_|  \__'_| |___/ |_| |_|          |_|     \___| | .__/   \___/ \n"
-printf "                                                        | |            \n"
-printf "                                                        |_|            \n"
-echo -e "\e[0m"
 
-#checking  If OS is Kali-Linux or other
+#functions 
+function kali(){
+    car='/tmp/fast-repo-dev'
+    mkdir $car 2>/dev/null && cd $car 2>/dev/null
+    curl -s http://http.kali.org/README.mirrorlist -k |grep "this country" -A 20|grep http|cut -d '/' -f3|sort -u > mirror
+    a=$(cat mirror)
+    echo -e  '\e[32mChecking the Best Mirror ;)  Hold on ヽ(´▽`)/.......  \e[0m'
+    echo -e '\n\n\n' 
 
-o=$(grep "Kali GNU/Linux" /etc/os-release -o|wc -m)
-if [[ "$o" == 0 ]] ; then echo This Script is for Kali-Linux OS && exit 1; fi
+    #checking the mirror latency 
+    for i in  $(cat mirror)
+    do
+    echo $(ping -c4 -w5 $i|grep rtt |cut -d '/' -f5)   $i  >> new 2>/dev/null & 
+    done
+    wait
+    #combine result 
+      
+    repo=$(cat new|grep -e [0-9]|sort -n|head -n1|awk '{print $2}') 
+    
+    sudo sed -i 's/^/#/g' "/etc/apt/sources.list"
+    echo "deb http://$repo/kali kali-rolling main contrib non-free" |sudo tee -a /etc/apt/sources.list >/dev/null 
+    echo -e "\n\n\n \e[93mFast Mirror Repo set\n\n \e[96mTry Now with \e[91msudo apt update    \n\e[95m @raoshaab :)"
+    sudo rm  -r $car 2>/dev/null
 
-#get all mirror list for ip 
-car='/tmp/fast-repo-dev'
+}
 
-mkdir $car 2>/dev/null && cd $car 2>/dev/null
-curl -s http://http.kali.org/README.mirrorlist -k |grep "this country" -A 20|grep http|cut -d '/' -f3 > mirror
-a=$(cat mirror)
-echo -e  '\e[32m Checking the Best Mirror ;)  Hold on ヽ(´▽`)/  \e[0m'
-echo -e '\n\n\n' 
-#checking the mirror latency 
-k=$(echo $a|wc -l)
-
-for i in  $(cat mirror)
-do
-ping -c4 $i|grep rtt |cut -d '/' -f5 >>new 
-echo -n "......."
-done
-
-#combine result 
-
-paste mirror new > final
-repo=$(cat final|awk '{print NF, $0}' | sort -k1,1rn -k3 -n | cut -d' ' -f2-|head -n1|awk '{print $1}')
-
-echo -e "\n\n\n \e[93m Now we have Set best Mirror Repo to you Kali-Linux \n\n Try Now  sudo apt update    \n Happy Hacking :) @raoshaab" 
+function ubuntu(){
+    car='/tmp/fast-repo-dev'
+    mkdir $car 2>/dev/null && cd $car 2>/dev/null
+    curl -sL http://mirrors.ubuntu.com/mirrors.txt > mirrors
+    mirror=$(curl -sL http://mirrors.ubuntu.com/mirrors.txt | cut -d '/' -f3|sort -u)
+    echo -e  '\e[32mChecking the Best Mirror ;)  Hold on ヽ(´▽`).......  \e[0m'
+    echo -e '\n\n\n' 
  
-sudo sed -i 's/^/#/g' "/etc/apt/sources.list"
-echo "deb http://$repo/kali kali-rolling main contrib non-free" |sudo tee -a /etc/apt/sources.list >/dev/null 
-sudo rm  -r $car 2>/dev/null
+
+    #checking the mirror latency 
+    for i in  $(echo $mirror)
+    do
+    echo $(ping -c4 -w5 $i|grep rtt |cut -d '/' -f5)   $i  >> new 2>/dev/null & 
+    done
+    wait
+    
+    repo=$(cat new|grep -e [0-9]|sort -n|head -n1|awk '{print $2}' ) 
+    #echo $repo
+    fast_server=$(cat mirrors|grep  $repo|head -n1) 
+    #echo $fast_server
+    
+    #setting the etc\apt\sources.list
+    code=$(lsb_release -c|awk '{print $2}')
+    sudo sed -i 's/^/#/g' "/etc/apt/sources.list"
+
+    echo "deb $fast_server $code main restricted " |sudo tee -a /etc/apt/sources.list >/dev/null 
+    echo "deb $fast_server $code-updates main restricted " |sudo tee -a /etc/apt/sources.list >/dev/null 
+    echo "deb $fast_server $code multiverse" |sudo tee -a /etc/apt/sources.list >/dev/null 
+    echo "deb $fast_server $code-updates multiverse" |sudo tee -a /etc/apt/sources.list >/dev/null 
+
+    echo -e "\n\n\n \e[93mFast Mirror Repo set\n\n \e[96mTry Now with \e[91msudo apt update    \n\e[95m @raoshaab :)"
+
+
+    sudo rm  -r $car 2>/dev/null
+}
+
+
+
+
+echo -e "\e[3$(( $RANDOM * 6 / 32767 + 1 ))m"
+
+bann="  
+  ____   _                 _                                            
+ / ___| | |               | |                                            
+| |_    | |   __ _   ___  | |__    ______   _ ___    ___    _ __      ___  
+|  _|   | |  / _  | / __| | '_ \  |__==__| | |_|_|  / _ \  | '_  \   / _ \ 
+| |     | | | (_| | \__ \ | | | |          | |     |  __/  | | _) | | (_) |
+|_|     |_|  \__'_| |___/ |_| |_|          |_|      \___|  | |___/   \___/ 
+                                                           | |             
+                                                           |_|            "
+
+while IFS= read -r -n 1 -d '' c; do   printf '\e[38;5;%dm%s\e[0m'  "$((RANDOM%255+1))" "$c"; done <<<$bann
+
+#checking for root access 
+if [ whoami == root  ]
+then 
+    echo ' '
+else 
+    echo -e '\n\n\t\t\t\t \e[91mRun with sudo!!' && exit 1
+fi
+
+#checking  If OS is Kali-Linux or Ubuntu
+os=$(cat /etc/os-release|grep "^ID="|cut -d '=' -f2)
+
+if [ $os == "kali" ]
+then 
+    kali
+elif [ $os == "ubuntu" ]
+then 
+    ubuntu
+else    
+    echo "OS not supported " && exit 1
+fi
